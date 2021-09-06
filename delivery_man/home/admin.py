0985b4afd11522123d0b5d04de785de62a7fae0b
Soumya Ranjan Patnaik <soumyaranjan1812@gmail.com>
2021-09-06 13:44:08 +0530

    Added models

--- delivery_man/home/admin.py
+++ delivery_man/home/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import * 
 
-# Register your models here.
+class WorkerAdmin(admin.ModelAdmin):
+	list_display = ('id', 'name', 'age', 'gender')
+
+class OrderAdmin(admin.ModelAdmin):
+	list_display = ('id','assignedTo', 'location', 'item', 'location', 'orderedBy')
+
+admin.site.register(Order, OrderAdmin)
+admin.site.register(Worker, WorkerAdmin)
